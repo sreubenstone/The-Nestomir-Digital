267a09049246e6f7652324c1c973b9f4f1411176
Steven Reubenstone <stevenreubenstone@Stevens-MacBook-Pro.local>
2020-01-31 19:24:15 -0500

    bookmark working pre refactor and performance considerations, no persistance yet

--- components/Book/Prologue.tsx
+++ components/Book/Prologue.tsx
@@ -8,7 +8,8 @@ import {
 } from "react-navigation";
 
 interface IState {
-  pos: number;
+  bookmark: number;
+  screenPos: number;
 }
 
 interface IProps {
@@ -20,24 +21,47 @@ const body1 = `It was a star filled night in Grapefield, Wisconsin. The sky look
 The cornfield next to Lemmingfield, the less up-kept part of Grapefield, was bristling  as a cool breeze came in from the north-west. It would be considered chilly tonight to anyone who grew up in these parts. But there were no souls in this cornfield on this night in Grapefield, Wisconsin. If there were, they would have seen something like a reddish-white firework exploding a few miles above them. They would have seen a great big fiery white streak coming down at them fast, very fast...and then they would have heard a big BOOOM.
 `;
 
+const testBody = `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.`;
+
 export default class Prologue extends Component<IProps, IState> {
   static navigationOptions = {
     header: null
   };
 
   state = {
-    pos: null
+    bookmark: null,
+    screenPos: null
+  };
+
+  setBookMark = () => {
+    const { bookmark } = this.state;
+    const { screenPos } = this.state;
+    if (bookmark) {
+      this.setState({ bookmark: 0 });
+      return;
+    }
+    this.setState({ bookmark: screenPos });
   };
 
   setPos = e => {
-    this.setState({ pos: e.nativeEvent.contentOffset.y });
+    this.setState({ screenPos: e.nativeEvent.contentOffset.y });
   };
 
   render() {
+    const { bookmark, screenPos } = this.state;
     const { navigation } = this.props;
+    const diff = Math.abs(screenPos - bookmark);
+    console.log("bookmark:", bookmark);
+    console.log("screenPos:", screenPos);
     return (
       <View style={{ position: "relative" }}>
-        <ScrollView style={[GlobalStyles.container1]} onScroll={this.setPos}>
+        <ScrollView
+          style={[GlobalStyles.container1]}
+          scrollEventThrottle={16}
+          onScroll={this.setPos}
+        >
           <Text style={GlobalStyles.chapterTitle}>Prologue</Text>
           <View style={GlobalStyles.flexCenter}>
             <Image
@@ -48,6 +72,8 @@ export default class Prologue extends Component<IProps, IState> {
             />
           </View>
           <Text style={BookStyles.bookFont}>{body1}</Text>
+          <Text style={BookStyles.bookFont}>{testBody}</Text>
+
           <TouchableOpacity
             onPress={() => navigation.navigate("StoryProgress")}
           >
@@ -63,10 +89,26 @@ export default class Prologue extends Component<IProps, IState> {
             justifyContent: "flex-end"
           }}
         >
-          <Image
-            source={require("../../assets/images/bookmarked.png")}
-            style={{ height: 70, width: 35, marginRight: 20, marginTop: 40 }}
-          />
+          <TouchableOpacity onPress={() => this.setBookMark()}>
+            <Image
+              source={
+                !bookmark
+                  ? // if no bookmark is set, show open icon.
+                    require("../../assets/images/bookmark.png")
+                  : diff < 50
+                  ? require("../../assets/images/bookmarked.png")
+                  : null // bookmark === true
+                // Case A) -- you are not at the scroll diff (show)
+                // Case B) -- you are not scrolled (hide image entirely)
+              }
+              style={{
+                height: 70,
+                width: 35,
+                marginRight: 20,
+                marginTop: 40
+              }}
+            />
+          </TouchableOpacity>
         </View>
       </View>
     );
