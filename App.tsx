14e70df1e194588b51be01e8bd999c2ef28930fa
Steven Reubenstone <stevenreubenstone@Stevens-MacBook-Pro.local>
2020-03-18 00:32:37 -0400

    beginning apollo client installation

--- App.tsx
+++ App.tsx
@@ -1,7 +1,24 @@
 import React, { Component } from "react";
 import * as Font from "expo-font";
+import * as SecureStore from "expo-secure-store";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
 import Navigator from "./navigation/MainTabNavigator";
 import SignUpContainer from "./components/Login/Container";
+import { GET_AUTH } from "./queries";
+import { useQuery } from "@apollo/react-hooks";
+
+const client = new ApolloClient({
+  uri: "https://1abf00bc.ngrok.io",
+  request: async operation => {
+    const token = await SecureStore.getItemAsync("jwt");
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ""
+      }
+    });
+  }
+});
 
 export default class App extends Component<{}> {
   state = {
@@ -20,6 +37,14 @@ export default class App extends Component<{}> {
 
   render() {
     const { fontLoaded, auth } = this.state;
-    return fontLoaded ? auth ? <Navigator /> : <SignUpContainer /> : null;
+    return fontLoaded ? (
+      auth ? (
+        <ApolloProvider client={client}>
+          <Navigator />
+        </ApolloProvider>
+      ) : (
+        <SignUpContainer />
+      )
+    ) : null;
   }
 }
